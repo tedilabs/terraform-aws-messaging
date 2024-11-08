4791e7f94597eed2845fb2606bae6f808f49e293
Byungjin Park (Claud) <posquit0.bj@gmail.com>
2024-11-08 17:23:56 +0900

    Up to date for msk-cluster module (#35)

--- modules/msk-cluster/security-group.tf
+++ modules/msk-cluster/security-group.tf
@@ -13,107 +13,120 @@ locals {
 
 module "security_group" {
   source  = "tedilabs/network/aws//modules/security-group"
-  version = "0.26.0"
+  version = "~> 0.32.0"
 
-  count = length(var.broker_allowed_ingress_cidrs) > 0 ? 1 : 0
-
-  name        = var.name
+  name        = "msk-${var.name}"
   description = "Security group for MSK Cluster."
   vpc_id      = local.vpc_id
 
-  ingress_rules = [
-    {
-      id          = "broker-plaintext/cidrs"
-      description = "Allow CIDRs to communicate with Kafka brokers in plaintext."
-      protocol    = "tcp"
-      from_port   = 9092
-      to_port     = 9092
+  ingress_rules = concat(
+    contains(["PLAINTEXT", "TLS_PLAINTEXT"], var.encryption_in_transit.client_mode) ? [
+      {
+        id          = "broker-plaintext/cidrs"
+        description = "Allow CIDRs to communicate with Kafka brokers in plaintext."
+        protocol    = "tcp"
+        from_port   = 9092
+        to_port     = 9092
 
-      cidr_blocks = var.broker_allowed_ingress_cidrs
-    },
-    {
-      id          = "broker-tls/cidrs"
-      description = "Allow CIDRs to communicate with Kafka brokers in tls."
-      protocol    = "tcp"
-      from_port   = 9094
-      to_port     = 9094
+        ipv4_cidrs = var.broker_allowed_ingress_cidrs
+      },
+    ] : [],
+    contains(["TLS", "TLS_PLAINTEXT"], var.encryption_in_transit.client_mode) ? [
+      {
+        id          = "broker-tls/cidrs"
+        description = "Allow CIDRs to communicate with Kafka brokers in tls."
+        protocol    = "tcp"
+        from_port   = 9094
+        to_port     = 9094
 
-      cidr_blocks = var.broker_allowed_ingress_cidrs
-    },
-    {
-      id          = "broker-sasl-scram/cidrs"
-      description = "Allow CIDRs to communicate with Kafka brokers in SASL SCRAM."
-      protocol    = "tcp"
-      from_port   = 9096
-      to_port     = 9096
+        ipv4_cidrs = var.broker_allowed_ingress_cidrs
+      },
+    ] : [],
+    var.authentication.sasl_scram.enabled ? [
+      {
+        id          = "broker-sasl-scram/cidrs"
+        description = "Allow CIDRs to communicate with Kafka brokers in SASL SCRAM."
+        protocol    = "tcp"
+        from_port   = 9096
+        to_port     = 9096
 
-      cidr_blocks = var.broker_allowed_ingress_cidrs
-    },
-    {
-      id          = "broker-sasl-iam/cidrs"
-      description = "Allow CIDRs to communicate with Kafka brokers in SASL IAM."
-      protocol    = "tcp"
-      from_port   = 9098
-      to_port     = 9098
+        ipv4_cidrs = var.broker_allowed_ingress_cidrs
+      },
+    ] : [],
+    var.authentication.sasl_iam.enabled ? [
+      {
+        id          = "broker-sasl-iam/cidrs"
+        description = "Allow CIDRs to communicate with Kafka brokers in SASL IAM."
+        protocol    = "tcp"
+        from_port   = 9098
+        to_port     = 9098
 
-      cidr_blocks = var.broker_allowed_ingress_cidrs
-    },
-    {
-      id          = "broker-public-tls/cidrs"
-      description = "Allow CIDRs to communicate with Kafka brokers in tls (public)."
-      protocol    = "tcp"
-      from_port   = 9194
-      to_port     = 9194
+        ipv4_cidrs = var.broker_allowed_ingress_cidrs
+      },
+    ] : [],
+    [
+      {
+        id          = "broker-public-tls/cidrs"
+        description = "Allow CIDRs to communicate with Kafka brokers in tls (public)."
+        protocol    = "tcp"
+        from_port   = 9194
+        to_port     = 9194
 
-      cidr_blocks = var.broker_allowed_ingress_cidrs
-    },
-    {
-      id          = "broker-public-sasl-scram/cidrs"
-      description = "Allow CIDRs to communicate with Kafka brokers in SASL SCRAM (public)."
-      protocol    = "tcp"
-      from_port   = 9196
-      to_port     = 9196
+        ipv4_cidrs = var.broker_allowed_ingress_cidrs
+      },
+      {
+        id          = "broker-public-sasl-scram/cidrs"
+        description = "Allow CIDRs to communicate with Kafka brokers in SASL SCRAM (public)."
+        protocol    = "tcp"
+        from_port   = 9196
+        to_port     = 9196
 
-      cidr_blocks = var.broker_allowed_ingress_cidrs
-    },
-    {
-      id          = "broker-public-sasl-iam/cidrs"
-      description = "Allow CIDRs to communicate with Kafka brokers in SASL IAM (public)."
-      protocol    = "tcp"
-      from_port   = 9198
-      to_port     = 9198
+        ipv4_cidrs = var.broker_allowed_ingress_cidrs
+      },
+      {
+        id          = "broker-public-sasl-iam/cidrs"
+        description = "Allow CIDRs to communicate with Kafka brokers in SASL IAM (public)."
+        protocol    = "tcp"
+        from_port   = 9198
+        to_port     = 9198
 
-      cidr_blocks = var.broker_allowed_ingress_cidrs
-    },
-    {
-      id          = "zookeeper/cidrs"
-      description = "Allow CIDRs to communicate with Kafka zookeepers."
-      protocol    = "tcp"
-      from_port   = 2181
-      to_port     = 2181
+        ipv4_cidrs = var.broker_allowed_ingress_cidrs
+      },
+      {
+        id          = "zookeeper/cidrs"
+        description = "Allow CIDRs to communicate with Kafka zookeepers."
+        protocol    = "tcp"
+        from_port   = 2181
+        to_port     = 2181
 
-      cidr_blocks = var.broker_allowed_ingress_cidrs
-    },
-    {
-      id          = "prometheus-jmx-exporter/cidrs"
-      description = "Allow CIDRs to communicate with Prometheus JMX Exporter."
-      protocol    = "tcp"
-      from_port   = 11001
-      to_port     = 11001
+        ipv4_cidrs = var.broker_allowed_ingress_cidrs
+      },
+    ],
+    var.prometheus.jmx_exporter_enabled ? [
+      {
+        id          = "prometheus-jmx-exporter/cidrs"
+        description = "Allow CIDRs to communicate with Prometheus JMX Exporter."
+        protocol    = "tcp"
+        from_port   = 11001
+        to_port     = 11001
 
-      cidr_blocks = var.broker_allowed_ingress_cidrs
-    },
-    {
-      id          = "prometheus-node-exporter/cidrs"
-      description = "Allow CIDRs to communicate with Prometheus Node Exporter."
-      protocol    = "tcp"
-      from_port   = 11002
-      to_port     = 11002
+        ipv4_cidrs = var.prometheus.allowed_ingress_ipv4_cidrs
+      },
+    ] : [],
+    var.prometheus.node_exporter_enabled ? [
+      {
+        id          = "prometheus-node-exporter/cidrs"
+        description = "Allow CIDRs to communicate with Prometheus Node Exporter."
+        protocol    = "tcp"
+        from_port   = 11002
+        to_port     = 11002
 
-      cidr_blocks = var.broker_allowed_ingress_cidrs
-    },
-  ]
+        ipv4_cidrs = var.prometheus.allowed_ingress_ipv4_cidrs
+      },
+    ] : [],
+  )
 
+  revoke_rules_on_delete = true
   resource_group_enabled = false
   module_tags_enabled    = false
 
--- modules/msk-cluster/variables.tf
+++ modules/msk-cluster/variables.tf
@@ -1,12 +1,13 @@
 variable "name" {
   description = "(Required) Name of the MSK cluster."
   type        = string
+  nullable    = false
 }
 
 variable "kafka_version" {
   description = "(Optional) Kafka version to use for the MSK cluster."
   type        = string
-  default     = "2.8.0"
+  default     = "3.6.0"
   nullable    = false
 }
 
@@ -17,9 +18,12 @@ variable "kafka_server_properties" {
   nullable    = false
 }
 
+# TODO: naming
+# TODO: validation
 variable "broker_size" {
   description = "(Required) The desired total number of broker nodes in the kafka cluster. It must be a multiple of the number of specified client subnets."
   type        = number
+  nullable    = false
 }
 
 variable "broker_instance_type" {
@@ -29,37 +33,10 @@ variable "broker_instance_type" {
   nullable    = false
 }
 
-variable "broker_volume_size" {
-  description = "(Optional) The size in GiB of the EBS volume for the data drive on each broker node. Minimum value of `1` and maximum value of `16384`. Defaults to `1000`."
-  type        = number
-  default     = 1000
-  nullable    = false
-
-  validation {
-    condition = alltrue([
-      var.broker_volume_size >= 1,
-      var.broker_volume_size <= 16384,
-    ])
-    error_message = "Valid value for `broker_volume_size` is between `1` and `16384`."
-  }
-}
-
-variable "broker_volume_provisioned_throughput_enabled" {
-  description = "(Optional) Whether provisioned throughput is enabled or not. You can specify the provisioned throughput rate in MiB per second for clusters whose brokers are of type `kafka.m5.4xlarge` or larger and if the storage volume is 10 GiB or greater. Defaults to `false`."
-  type        = bool
-  default     = false
-  nullable    = false
-}
-
-variable "broker_volume_provisioned_throughput" {
-  description = "(Optional) Throughput value of the EBS volumes for the data drive on each kafka broker node in MiB per second. The minimum value is `250`. The maximum value varies between broker type."
-  type        = number
-  default     = null
-}
-
 variable "broker_subnets" {
   description = "(Required) A list of subnet IDs to place ENIs of the MSK cluster broker nodes within."
   type        = list(string)
+  nullable    = false
 }
 
 variable "broker_public_access_enabled" {
@@ -72,161 +49,196 @@ variable "broker_public_access_enabled" {
 variable "broker_allowed_ingress_cidrs" {
   description = "(Optional) A list of CIDR for MSK ingress access."
   type        = list(string)
-  default     = []
+  default     = ["10.0.0.0/8"]
+  nullable    = false
 }
 
 variable "broker_additional_security_groups" {
   description = "(Optional) A list of security group IDs to associate with ENIs to control who can communicate with the cluster."
   type        = list(string)
   default     = []
-}
-
-variable "auth_unauthenticated_access_enabled" {
-  description = "(Optional) Enables unauthenticated access. Defaults to `true`."
-  type        = bool
-  default     = true
   nullable    = false
 }
 
-variable "auth_sasl_iam_enabled" {
-  description = "(Optional) Enables IAM client authentication."
-  type        = bool
-  default     = false
-  nullable    = false
-}
-
-variable "auth_sasl_scram_enabled" {
-  description = "(Optional) Enables SCRAM client authentication via AWS Secrets Manager."
-  type        = bool
-  default     = false
-  nullable    = false
-}
-
-variable "auth_sasl_scram_kms_key" {
-  description = "(Optional) The ARN of a KMS key to encrypt AWS SeecretsManager Secret resources for storing SASL/SCRAM authentication data. Only required when the MSK cluster has SASL/SCRAM authentication enabled. The Username/Password Authentication based on SASL/SCRAM needs to create a Secret resource in AWS SecretsManager with a custom AWS KMS Key. A secret created with the default AWS KMS key cannot be used with an Amazon MSK cluster."
-  type        = string
-  default     = null
-}
-
-variable "auth_sasl_scram_users" {
-  description = "(Optional) A list of usernames to be allowed for SASL/SCRAM authentication to the MSK cluster. The password for each username is randomly generated and stored in AWS SecretsManager secret."
-  type        = set(string)
-  default     = []
-  nullable    = false
-}
-
-variable "auth_tls_enabled" {
-  description = "(Optional) Enables TLS client authentication."
-  type        = bool
-  default     = false
-  nullable    = false
-}
-
-variable "auth_tls_acm_ca_arns" {
-  description = "(Optional) List of ACM Certificate Authority Amazon Resource Names (ARNs)."
-  type        = list(string)
-  default     = []
-  nullable    = false
-}
-
-variable "encryption_at_rest_kms_key" {
-  description = "(Optional) Specify a KMS key short ID or ARN (it will always output an ARN) to use for encrypting your data at rest. If no key is specified, an AWS managed KMS ('aws/msk' managed service) key will be used for encrypting the data at rest."
-  type        = string
-  default     = ""
-}
-
-variable "encryption_in_transit_in_cluster_enabled" {
-  description = "(Optional) Whether data communication among broker nodes is encrypted."
-  type        = bool
-  default     = true
-  nullable    = false
-}
-
-variable "encryption_in_transit_client_mode" {
-  description = "(Optional) Encryption setting for data in transit between clients and brokers. `TLS`, `TLS_PLAINTEXT`, `PLAINTEXT` are available."
-  type        = string
-  default     = "TLS_PLAINTEXT"
-  nullable    = false
+variable "broker_storage" {
+  description = <<EOF
+  (Optional) The configuration for broker storage of the MSK cluster. `broker_storage` is defined below.
+    (Optional) `volume_size` - The size in GiB of the EBS volume for the data drive on each broker node. Minimum value of `1` and maximum value of `16384`. Defaults to `1000`.
+    (Optional) `provisioned_throughput` - The configuration for provisioned throughput of the EBS volume. `provisioned_throughput` is defined below.
+      (Optional) `enabled` - Whether provisioned throughput is enabled or not. You can specify the provisioned throughput rate in MiB per second for clusters whose brokers are of type `kafka.m5.4xlarge` or larger and if the storage volume is 10 GiB or greater. Defaults to `false`.
+      (Optional) `throughput` - Throughput value of the EBS volumes for the data drive on each kafka broker node in MiB per second. The minimum value is `250`. The maximum value varies between broker type.
+  EOF
+  type = object({
+    volume_size = optional(number, 1000)
+    provisioned_throughput = optional(object({
+      enabled    = optional(bool, false)
+      throughput = optional(number)
+    }), {})
+  })
+  default  = {}
+  nullable = false
 
   validation {
-    condition     = contains(["TLS", "TLS_PLAINTEXT", "PLAINTEXT"], var.encryption_in_transit_client_mode)
-    error_message = "Valid values are `TLS`, `TLS_PLAINTEXT`, `PLAINTEXT`."
+    condition = alltrue([
+      var.broker_storage.volume_size >= 1,
+      var.broker_storage.volume_size <= 16384,
+    ])
+    error_message = "Valid value for `volume_size` is between `1` and `16384`."
+  }
+  validation {
+    condition = anytrue([
+      var.broker_storage.provisioned_throughput.enabled == false,
+      var.broker_storage.provisioned_throughput.enabled == true && var.broker_storage.provisioned_throughput.throughput >= 250,
+    ])
+    error_message = "Valid value for `throughput` is greater than `250`."
   }
 }
 
-variable "logging_cloudwatch_enabled" {
-  description = "(Optional) Indicates whether you want to enable or disable streaming broker logs to Cloudwatch Logs."
-  type        = bool
-  default     = false
-  nullable    = false
-}
-
-variable "logging_cloudwatch_log_group" {
-  description = "(Optional) The name of log group on CloudWatch Logs to deliver logs to."
+variable "cluster_storage_mode" {
+  description = <<EOF
+  (Optional) Controls storage mode for supported storage tiers. Valid values are are `LOCAL` or `TIERED`. Defaults to `LOCAL`.
+    `LOCAL` - Storage mode that uses local storage for all topics.
+    `TIERED` - Storage mode that uses remote, longer-term tiered storage for certain topics at a lower cost. A remote tier automatically expands your storage capacity if necessary.
+  EOF
   type        = string
-  default     = ""
-  nullable    = false
-}
-
-variable "logging_firehose_enabled" {
-  description = "(Optional) Indicates whether you want to enable or disable streaming broker logs to Kinesis Data Firehose."
-  type        = bool
-  default     = false
-  nullable    = false
-}
-
-variable "logging_firehose_delivery_stream" {
-  description = "(Optional) Name of the Kinesis Data Firehose delivery stream to deliver logs to."
-  type        = string
-  default     = ""
-  nullable    = false
-}
-
-variable "logging_s3_enabled" {
-  description = "(Optional) Indicates whether you want to enable or disable streaming broker logs to S3."
-  type        = bool
-  default     = false
-  nullable    = false
-}
-
-variable "logging_s3_bucket" {
-  description = "(Optional) The name of the S3 bucket to deliver logs to."
-  type        = string
-  default     = ""
-  nullable    = false
-}
-
-variable "logging_s3_prefix" {
-  description = "(Optional) The prefix to append to the folder name."
-  type        = string
-  default     = ""
-  nullable    = false
-}
-
-variable "monitoring_cloudwatch_level" {
-  description = "(Optional) The desired enhanced MSK CloudWatch monitoring level. `DEFAULT`, `PER_BROKER`, `PER_TOPIC_PER_BROKER`, `PER_TOPIC_PER_PARTITION` are available."
-  type        = string
-  default     = "DEFAULT"
+  default     = "LOCAL"
   nullable    = false
 
   validation {
-    condition     = contains(["DEFAULT", "PER_BROKER", "PER_TOPIC_PER_BROKER", "PER_TOPIC_PER_PARTITION"], var.monitoring_cloudwatch_level)
-    error_message = "Valid values are `DEFAULT`, `PER_BROKER`, `PER_TOPIC_PER_BROKER`, `PER_TOPIC_PER_PARTITION`."
+    condition     = contains(["LOCAL", "TIERED"], var.cluster_storage_mode)
+    error_message = "Valid values for `cluster_storage_mode` are `LOCAL` or `TIERED`."
   }
 }
 
-variable "monitoring_prometheus_jmx_exporter_enabled" {
-  description = "(Optional) Indicates whether you want to enable or disable the JMX Exporter."
-  type        = bool
-  default     = false
-  nullable    = false
+variable "authentication" {
+  description = <<EOF
+  The configuration for authentication of the MSK cluster. `authentication` is defined below.
+    (Optional) `unauthenticated_access.enabled` - Whether to enable unauthenticated access. Defaults to `true`.
+    (Optional) `sasl_iam.enabled` - Whether to enable IAM client authentication. Defaults to `false`.
+    (Optional) `sasl_scram` - The configuration for SASL/SCRAM client authentication. `sasl_scram` is defined below.
+      (Optional) `enabled` - Whether to enable SCRAM client authentication via AWS Secrets Manager. Defaults to `false`.
+      (Optional) `kms_key` - The ARN of a KMS key to encrypt AWS SecretsManager Secret resources for storing SASL/SCRAM authentication data. Only required when the MSK cluster has SASL/SCRAM authentication enabled. The Username/Password Authentication based on SASL/SCRAM needs to create a Secret resource in AWS SecretsManager with a custom AWS KMS Key. A secret created with the default AWS KMS key cannot be used with an Amazon MSK cluster.
+      (Optional) `users` - A list of usernames to be allowed for SASL/SCRAM authentication to the MSK cluster. The password for each username is randomly generated and stored in AWS SecretsManager secret.
+    (Optional) `tls` - The configuration for TLS client authentication. `tls` is defined below.
+      (Optional) `enabled` - Whether to enable TLS client authentication. Defaults to `false`.
+      (Optional) `acm_private_certificate_authorities` - A list of ACM Private CA ARNs to enable client authentication.
+  EOF
+  type = object({
+    unauthenticated_access = optional(object({
+      enabled = optional(bool, true)
+    }), {})
+    sasl_iam = optional(object({
+      enabled = optional(bool, false)
+    }), {})
+    sasl_scram = optional(object({
+      enabled = optional(bool, false)
+      kms_key = optional(string)
+      users   = optional(set(string), [])
+    }), {})
+    tls = optional(object({
+      enabled                             = optional(bool, false)
+      acm_private_certificate_authorities = optional(list(string), [])
+    }), {})
+  })
+  default  = {}
+  nullable = false
 }
 
-variable "monitoring_prometheus_node_exporter_enabled" {
-  description = "(Optional) Indicates whether you want to enable or disable the Node Exporter."
-  type        = bool
-  default     = false
-  nullable    = false
+variable "encryption_at_rest" {
+  description = <<EOF
+  The configuration for at-rest encryption of the MSK cluster. `encryption_at_rest` is defined below.
+    (Optional) `kms_key` - The short ID or ARN of the KMS key to use for at-rest encryption. If not supplied, uses AWS managed encryption key.
+  EOF
+  type = object({
+    kms_key = optional(string)
+  })
+  default  = {}
+  nullable = false
+}
+
+variable "encryption_in_transit" {
+  description = <<EOF
+  The configuration for in-transit encryption of the MSK cluster.
+    (Optional) `in_cluster_enabled` - Whether data communication among broker nodes is encrypted. Defaults to `true`.
+    (Optional) `client_mode` - Encryption setting for data in transit between clients and brokers. Valid values are `TLS`, `TLS_PLAINTEXT`, `PLAINTEXT`. Defaults to `TLS_PLAINTEXT`.
+  EOF
+  type = object({
+    in_cluster_enabled = optional(bool, true)
+    client_mode        = optional(string, "TLS_PLAINTEXT")
+  })
+  default  = {}
+  nullable = false
+
+  validation {
+    condition     = contains(["TLS", "TLS_PLAINTEXT", "PLAINTEXT"], var.encryption_in_transit.client_mode)
+    error_message = "Valid values for `client_mode` are `TLS`, `TLS_PLAINTEXT`, `PLAINTEXT`."
+  }
+}
+
+variable "logging" {
+  description = <<EOF
+  The configuration for logging of the MSK cluster. `logging` is defined below.
+    (Optional) `cloudwatch_logs` - The configuration for logging with CloudWatch Logs of the MSK cluster. `cloudwatch_logs` is defined below.
+      (Optional) `enabled` - Whether to enable or disable streaming broker logs to Cloudwatch Logs.
+      (Optional) `log_group` - The name of the CloudWatch Log Group to deliver logs to.
+    (Optional) `firehose` - The configuration for logging with Kinesis Data Firehose of the MSK cluster. `firehose` is defined below.
+      (Optional) `enabled` - Whether to enable or disable streaming broker logs to Kinesis Data Firehose.
+      (Optional) `delivery_stream` - The name of the Kinesis Data Firehose delivery stream to deliver logs to.
+    (Optional) `s3` - The configuration for logging with S3 of the MSK cluster. `s3` is defined below.
+      (Optional) `enabled` - Whether to enable or disable streaming broker logs to S3.
+      (Optional) `bucket` - The name of the S3 bucket to deliver logs to.
+      (Optional) `key_prefix` - The prefix to append to the folder name.
+  EOF
+  type = object({
+    cloudwatch_logs = optional(object({
+      enabled   = optional(bool, false)
+      log_group = optional(string)
+    }), {})
+    firehose = optional(object({
+      enabled         = optional(bool, false)
+      delivery_stream = optional(string)
+    }), {})
+    s3 = optional(object({
+      enabled    = optional(bool, false)
+      bucket     = optional(string)
+      key_prefix = optional(string)
+    }), {})
+  })
+  default  = {}
+  nullable = false
+}
+
+variable "cloudwatch_metrics" {
+  description = <<EOF
+  The configuration for CloudWatch Metrics of the MSK cluster. `cloudwatch_mterics` is defined below.
+    (Optional) `monitoring_level` - The desired enhanced MSK CloudWatch monitoring level. `DEFAULT`, `PER_BROKER`, `PER_TOPIC_PER_BROKER`, `PER_TOPIC_PER_PARTITION` are available. Defaults to `DEFAULT`.
+  EOF
+  type = object({
+    monitoring_level = optional(string, "DEFAULT")
+  })
+  default  = {}
+  nullable = false
+
+  validation {
+    condition     = contains(["DEFAULT", "PER_BROKER", "PER_TOPIC_PER_BROKER", "PER_TOPIC_PER_PARTITION"], var.cloudwatch_metrics.monitoring_level)
+    error_message = "Valid values for `monitoring_level` are `DEFAULT`, `PER_BROKER`, `PER_TOPIC_PER_BROKER`, `PER_TOPIC_PER_PARTITION`."
+  }
+}
+
+variable "prometheus" {
+  description = <<EOF
+  The configuration for Open Monitoring with Prometheus of the MSK cluster. `prometheus` is defined below.
+    (Optional) `jmx_exporter_enabled` - Whether to enable or disable the JMX Exporter.
+    (Optional) `node_exporter_enabled` - Whether to enable or disable the Node Exporter.
+    (Optional) `allowed_ingress_ipv4_cidrs` - A list of IPv4 CIDRs to allow ingress access to Prometheus endpoint.
+  EOF
+  type = object({
+    jmx_exporter_enabled       = optional(bool, false)
+    node_exporter_enabled      = optional(bool, false)
+    allowed_ingress_ipv4_cidrs = optional(set(string), ["10.0.0.0/8"])
+  })
+  default  = {}
+  nullable = false
 }
 
 variable "timeouts" {
@@ -263,16 +275,19 @@ variable "resource_group_enabled" {
   description = "(Optional) Whether to create Resource Group to find and group AWS resources which are created by this module."
   type        = bool
   default     = true
+  nullable    = false
 }
 
 variable "resource_group_name" {
   description = "(Optional) The name of Resource Group. A Resource Group name can have a maximum of 127 characters, including letters, numbers, hyphens, dots, and underscores. The name cannot start with `AWS` or `aws`."
   type        = string
   default     = ""
+  nullable    = false
 }
 
 variable "resource_group_description" {
   description = "(Optional) The description of Resource Group."
   type        = string
   default     = "Managed by Terraform."
+  nullable    = false
 }
