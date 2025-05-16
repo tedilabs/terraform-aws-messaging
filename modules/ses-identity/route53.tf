db0f3e210f454fd2717f3a2d600002909eb4acd4
Byungjin Park <posquit0.bj@gmail.com>
2025-05-17 03:35:23 +0900

    feat(ses-identity): fix timing issue for dkim dns record

--- modules/ses-identity/route53.tf
+++ modules/ses-identity/route53.tf
@@ -1,18 +1,25 @@
+locals {
+  dkim_verification_enabled = local.identity_type == "DOMAIN" && var.dkim.type == "EASY_DKIM" && var.dkim.verification.enabled
+  tokens = (local.dkim_verification_enabled
+    ? aws_sesv2_email_identity.this.dkim_signing_attributes[0].tokens
+    : []
+  )
+}
+
+
 ###################################################
 # DKIM Verification
 ###################################################
 
 resource "aws_route53_record" "dkim" {
-  for_each = (aws_sesv2_email_identity.this.identity_type == "DOMAIN" && var.dkim.type == "EASY_DKIM" && var.dkim.verification.enabled
-    ? toset(aws_sesv2_email_identity.this.dkim_signing_attributes[0].tokens)
-    : []
-  )
+  count = local.dkim_verification_enabled ? 3 : 0
 
   zone_id = var.dkim.verification.zone_id
-  name    = "${each.key}._domainkey.${aws_sesv2_email_identity.this.email_identity}"
+  name    = "${local.tokens[count.index]}._domainkey.${aws_sesv2_email_identity.this.email_identity}"
   type    = "CNAME"
-  records = ["${each.key}.dkim.amazonses.com"]
+  records = ["${local.tokens[count.index]}.dkim.amazonses.com"]
 
   ttl             = "600"
   allow_overwrite = true
 }
+
