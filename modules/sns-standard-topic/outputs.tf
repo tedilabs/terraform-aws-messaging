95e0686626b41bc5810f482333a0506b42e09072
Byungjin Park (Claud) <posquit0.bj@gmail.com>
2025-09-11 00:26:59 +0900

    feat: improve how to use resource_group in modules (#37)

--- modules/sns-standard-topic/outputs.tf
+++ modules/sns-standard-topic/outputs.tf
@@ -179,3 +179,19 @@ output "encryption_at_rest" {
 #     }
 #   }
 # }
+
+output "resource_group" {
+  description = "The resource group created to manage resources in this module."
+  value = merge(
+    {
+      enabled = var.resource_group.enabled && var.module_tags_enabled
+    },
+    (var.resource_group.enabled && var.module_tags_enabled
+      ? {
+        arn  = module.resource_group[0].arn
+        name = module.resource_group[0].name
+      }
+      : {}
+    )
+  )
+}
