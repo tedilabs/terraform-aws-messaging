d51a9f540a41dc1edd3193cd5cfb59448d53f1d8
Byungjin Park <posquit0.bj@gmail.com>
2026-08-11 17:37:55 +0900

    fix: replace deprecated aws_region name attribute with region
    The AWS provider deprecated the `name` attribute of the `aws_region` data
    source in v6.0.0 in favour of `region`, so `terraform validate` emitted a
    deprecation warning at both call sites. Both dirs already floor at aws >= 6.12
    and ~> 6.0 respectively, so `.region` is available without any constraint
    change.
    
    - modules/eventbridge-rule/iam.tf:8
    - examples/eventbridge/main.tf:10

--- modules/eventbridge-rule/iam.tf
+++ modules/eventbridge-rule/iam.tf
@@ -5,7 +5,7 @@ data "aws_region" "this" {}
 locals {
   partition  = data.aws_partition.this.partition
   account_id = data.aws_caller_identity.this.account_id
-  region     = data.aws_region.this.name
+  region     = data.aws_region.this.region
 }
 
 
