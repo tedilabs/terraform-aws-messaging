9121186798f2c199f6c5a16c75aa137ae770c0b5
Byungjin Park (Claud) <posquit0.bj@gmail.com>
2026-08-11 23:06:28 +0900

    fix: replace deprecated aws_region name attribute with region (#58)

--- examples/eventbridge/main.tf
+++ examples/eventbridge/main.tf
@@ -7,7 +7,7 @@ data "aws_region" "this" {}
 
 locals {
   account_id = data.aws_caller_identity.this.account_id
-  region     = data.aws_region.this.name
+  region     = data.aws_region.this.region
 }
 
 
